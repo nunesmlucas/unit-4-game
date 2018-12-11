8e573e67c091987076bd33e8a7d7b03af6a996cd
Lucas Nunes <nunesmlucasg@gmail.com>
2018-12-11 13:20:11 -0500

    small chunk

--- assets/game.js
+++ assets/game.js
@@ -1,17 +1,32 @@
+$(document).ready(function() {
+
 var randomScore = 0;
 var crystalNumbers = [10, 8, 3, 5];
-var blueCrystal = 0;
-var greenCrystal = 0;
-var redCrystal = 0;
-var purpleCrystal = 0;
+// var blueCrystal = 0;
+// var greenCrystal = 0;
+// var redCrystal = 0;
+// var purpleCrystal = 0;
+var crystalColors = ["blueCrystal", "greenCrystal", "purpleCrystal", "redCrystal"];
+var imgArray = new Array();
+imgArray[0] = new Image();
+imgArray[0].src = '../unit-4-game/assets/images/redCrystal.png';
+
+imgArray[1] = new Image();
+imgArray[1].src = '../unit-4-game/assets/images/blueCrystal.png';
+
+imgArray[2] = new Image();
+imgArray[2].src = '../unit-4-game/assets/images/greenCrystal.png';
+
+imgArray[3] = new Image();
+imgArray[3].src = '../unit-4-game/assets/images/purpleCrystal.png';
+
+var currentScore = 0;
 
 
 
-initialize();
+initialize = function () {
 
-function initialize() {
-
-    var currentScore = 0;
+    currentScore = "";
 
     randomScore = Math.floor(Math.random() * 50) + 1;
     console.log(randomScore);
@@ -27,28 +42,29 @@ function initialize() {
     };
     console.log(crystalObj);
 
-
     // USE THIS TO CREATE IMG's THEN SET VALUES IN ORDER TO DO THE MATH. 12.html
 
-    // for (var i = 0; i < numberOptions.length; i++) {
+    for (var i = 0; i < tempArray.length; i++) {
 
-    //     // For each iteration, we will create an imageCrystal
-    //     var imageCrystal = $("<img>");
-    
-    //     // First each crystal will be given the class ".crystal-image".
-    //     // This will allow the CSS to take effect.
-    //     imageCrystal.addClass("crystal-image");
-    
-    //     // Each imageCrystal will be given a src link to the crystal image
-    //     imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-    
-    //     // Each imageCrystal will be given a data attribute called data-crystalValue.
-    //     // This data attribute will be set equal to the array value.
-    //     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-    
-    //     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    //     $("#crystals").append(imageCrystal);
-    //   }
+        // For each iteration, we will create an imageCrystal
+        var imageCrystal = $("<img>");
+
+        // First each crystal will be given the class ".crystal-image".
+        // This will allow the CSS to take effect.
+        imageCrystal.addClass("crystal-image");
+
+        // Each imageCrystal will be given a src link to the crystal image
+        imageCrystal.attr("src", imgArray[i].src);
+
+        // Each imageCrystal will be given a data attribute called data-crystalValue.
+        // This data attribute will be set equal to the array value.
+        imageCrystal.attr("value", tempArray[i]);
+        imageCrystal.attr("data-crystalColor", crystalColors[i]);
+        imageCrystal.attr("class", "crystalImage");
+
+        // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+        $("#crystals").append(imageCrystal);
+    }
 
     function shuffle(a) {
         var j, x, i;
@@ -61,13 +77,19 @@ function initialize() {
         return a;
     }
 
-
-    // $('.crystalImage').on('click', function (event) {
-    //     console.log(this);
-    //     console.log($(this).val());
-    //     currentScore += $(this).val();
-    //     // display the new number into result box
-    //     $('#current-score').text(currentScore);
-    // });
-
 };
+initialize();
+
+
+$('.crystalImage').on('click', function (event) {
+    console.log(this);
+    console.log($(this).val());
+    currentScore += $(this).val();
+    // display the new number into result box
+    $('#current-score').text(currentScore);
+    console.log(currentScore);
+});
+
+
+
+});
