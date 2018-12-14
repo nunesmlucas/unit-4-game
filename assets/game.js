68230494b9a908889c26c9acbef03a97079ac03a
Lucas Nunes <nunesmlucasg@gmail.com>
2018-12-14 15:16:20 -0500

    final 1.0

--- assets/game.js
+++ assets/game.js
@@ -34,6 +34,7 @@ function initialize() {
     $('#number-to-guess').empty();
 
     currentScore = 0;
+    $('#current-score').text(currentScore);
 
     randomScore = Math.floor(Math.random() * 30) + 20;
     console.log(randomScore);
@@ -107,18 +108,81 @@ $('#crystals').on('click', '.crystalImage', function (event) {
         console.log(wins);
         $("#wins-score").text(wins);
 
-        alert("You have matched the score and WON!");
+        alert("You have matched the score and WON! The score was: " + randomScore);
         initialize();
     }
     else if (currentScore > randomScore) {
         losses++;
         console.log(losses);
         $("#losses-score").text(losses);
-        alert("You went over the score and LOSE! :(");
+        alert("You went over the score and LOSE! The Score was: " + randomScore);
         initialize();
     }
 
 });
+
+$('#reset-button').on('click', function () {
+    hardReset();
+});
+
+
+function hardReset() {
+
+    wins = 0;
+    losses = 0;
+
+    $('#crystals').empty();
+    $('#number-to-guess').empty();
+    $("#wins-score").text(0);
+    $("#losses-score").text(0);
+
+    currentScore = 0;
+    $('#current-score').text(currentScore);
+
+    randomScore = Math.floor(Math.random() * 30) + 20;
+    console.log(randomScore);
+
+    tempArray = shuffle(crystalNumbers);
+    console.log(shuffle(crystalNumbers));
+
+    var crystalObj = {
+        red: tempArray[0],
+        blue: tempArray[1],
+        green: tempArray[2],
+        purple: tempArray[3]
+    };
+    console.log(crystalObj);
+    console.log("HERE 1st.");
+
+
+    for (var i = 0; i < tempArray.length; i++) {
+        console.log("HERE 2nd FOOR LOOP.");
+
+        // For each iteration, we will create an imageCrystal
+        var imageCrystal = $("<img>");
+
+        // First each crystal will be given the class ".crystal-image".
+        // This will allow the CSS to take effect.
+        imageCrystal.addClass("crystal-image");
+
+        // Each imageCrystal will be given a src link to the crystal image
+        imageCrystal.attr("src", imgArray[i].src);
+        console.log(imgArray[i].src);
+
+        // Each imageCrystal will be given a data attribute called data-crystalValue.
+        // This data attribute will be set equal to the array value.
+        imageCrystal.attr("data-crystalValue", tempArray[i]);
+        // imageCrystal.attr("data-crystalColor", crystalColors[i]);
+        imageCrystal.attr("class", "crystalImage");
+
+        // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+        $("#crystals").append(imageCrystal);
+
+
+    }
+    $("#number-to-guess").text(randomScore);
+
+};
 
 
 
